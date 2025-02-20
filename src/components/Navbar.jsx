a6236c52ed0679fc1a4093da16b19cbb28e27dbb
developmentjc <developmentjc@outlook.com>
2025-02-20 16:48:53 -0600

    navbar codification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div>
+            Navbar
+            <li>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/login"}>LogIn</Link>
+            </li>
+        </div>
+    )
 }
 
 export default Navbar
