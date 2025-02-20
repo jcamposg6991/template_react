fa9675b0b2c797f04303ca91ca118277f6a99f23
developmentjc <developmentjc@outlook.com>
2025-02-20 16:42:48 -0600

    route configuration for logIn view

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from "./views/Home.jsx"
 import NotFound from "./views/NotFound.jsx"
+import LogIn from './views/LogIn.jsx'
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
         <Routes>
           <Route exact path='/' element={<Home/>}></Route>
           <Route exact path='/*' element={<NotFound/>}></Route>
+          <Route exact path='/login' element={<LogIn/>}></Route>
         </Routes>
       </BrowserRouter>
 
