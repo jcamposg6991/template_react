7ad3e1c68970865f81dae59df024263c35f88af8
developmentjc <developmentjc@outlook.com>
2025-02-20 16:31:48 -0600

    home and notfound views import into Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./views/Home.jsx"
+import NotFound from "./views/NotFound.jsx"
 
 const Layout = () => {
   return (
