cf3384d3ca2c6ab749c5363570347b933cb5dd75
developmentjc <developmentjc@outlook.com>
2025-02-25 16:03:11 -0600

    implementacion del useNavigate en Home.jsx dentro de la funcion handlerClick

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+
+  const handlerClick = () =>{
+    console.log("estoy funcionando");
+    
+  }
+
   return (
     <div>
       Home
       <Navbar/>
+      <button onClick={handlerClick}></button>
     </div>
   )
 }
