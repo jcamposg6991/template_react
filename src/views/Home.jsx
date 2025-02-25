715e24b56b3e2bee5b53ab9e958ee1b4ba09f934
developmentjc <developmentjc@outlook.com>
2025-02-25 16:40:55 -0600

    importacion de useContext y Contex en el home.jsx para poder utilizar los valores que se encuentran en el flux.js

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { Context } from '../js/store/appContext.js'
 import Navbar from '../components/Navbar.jsx'
 import { useNavigate } from 'react-router-dom'
 
