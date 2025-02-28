2e3faaa4789b3bfd0f2d0ea526367199958c0d4d
developmentjc <developmentjc@outlook.com>
2025-02-28 16:05:03 -0600

    codificacion de action Register en el flux

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,13 +1,43 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			personas: ["Pedro","Maria"]
+			personas: ["Pedro", "Maria"]
 		},
 		actions: {
 
 			exampleFunction: () => {
-                    console.log("hola")
-                    return
+				console.log("hola")
+				return
+			},
+
+			register: async (name, email, password) => {
+				try {
+					const data = {
+						name: name,
+						email: email,
+						password: password
+					};
+
+					const response = await fetch("http://127.0.0.1:5000/admin/users", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify(data)
+					});
+
+					const statusCode = response.status;
+					const responseData = await response.json();
+
+					if (statusCode === 201) {
+						setStore({ ...getStore(), registerStatus: true });
+					}
+
+					return responseData;
+				} catch (error) {
+					console.error("Error:", error);
+					throw error;
+				}
 			},
 		}
 	};
